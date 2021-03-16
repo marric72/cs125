fffc88e3cf1f4886890e0dc710fc0ee7a481fb9b
Heather Marriott <marric72@erau.edu>
2021-03-15 22:30:51 -0700

    Adding a new file

---
+++ helloWorld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+  char initial;    int age;    float weight;
+  printf("Enter your age: ");
+  scanf("%d", &age);
+  printf("Enter your weight: ");
+  scanf("%f", &weight);
+  printf("Enter your letter: ");
+  scanf(" %c", &initial);
+
+  printf("Initial %c(ascii %d), Age %d, Weight %.1f\n",
+         initial, initial, age, weight);
+  printf("Test addition int+char %d\n", age+initial);
+  printf("Test addition weight+int %.1f\n", age+weight);
+  printf("Test addition weight+char %.2f\n", initial+weight);
+  return 0;
+}
+
